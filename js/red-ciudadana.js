47ab742d5beeacb5f8900bb4b7a25c64f4525628
agarria <agarria@icloud.com>
2020-06-08 18:12:45 -0600

    overall improvements

---
+++ js/red-ciudadana.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    
+    $('.header').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots: false,
+        arrows: true,
+        adaptiveHeight: true,
+        autoplaySpeed: 3000,
+    });
+    
+});
